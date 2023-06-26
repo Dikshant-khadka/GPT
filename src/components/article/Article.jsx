aae9192ecb070ccd8cb0841cdc51986abd763225
latentry31 <khadkadikshant@gmail.com>
2023-06-27 06:18:08 +1000

    first commit

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './article.css'
+
+
+const Article = ({imageUrl, date, title}) => {
+  return (
+    <div className='gpt3__blog-container_article'>
+      <div className="gpt3__blog-container_article-image">
+        <img src={imageUrl} alt="blog" />
+
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <p>{title}</p>
+        </div>
+        <div>
+          <p>Read Full Article</p>
+        </div>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Article;
